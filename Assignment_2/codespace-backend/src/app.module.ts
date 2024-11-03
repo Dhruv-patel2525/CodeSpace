c8b5b6fa4902cc935f6d9ae29d07ed60a8f6392a
Vishwa Raman <viswa3388@gmail.com>
2024-11-03 16:09:06 -0500

    Created_submission_module

--- Assignment_2/codespace-backend/src/app.module.ts
+++ Assignment_2/codespace-backend/src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './users/users.module';
 import { CoursesModule } from './courses/courses.module';
 import { ProblemModule } from './problem/problem.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { SubmissionsModule } from './submissions/submissions.module';
 
 @Module({
-  imports: [UsersModule, CoursesModule, ProblemModule,MongooseModule.forRoot('mongodb://localhost:27017/codespace-data')],
+  imports: [UsersModule, CoursesModule, ProblemModule,MongooseModule.forRoot('mongodb://localhost:27017/codespace-data'), SubmissionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
