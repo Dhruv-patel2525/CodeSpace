22ebef53ef1ef973b8c3f66650947c419e49bfa3
Dhruvipatel2525 <www.dp454@gmail.com>
2024-11-11 19:05:49 -0500

    Added auth module

--- Assignment_3/codespace-backend/src/app.module.ts
+++ Assignment_3/codespace-backend/src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './users/users.module';
 import { CoursesModule } from './courses/courses.module';
 import { ProblemModule } from './problem/problem.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, CoursesModule, ProblemModule,MongooseModule.forRoot('mongodb://localhost:27017/codespace-data')],
+  imports: [UsersModule, CoursesModule, ProblemModule,MongooseModule.forRoot('mongodb://localhost:27017/codespace-data'), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
