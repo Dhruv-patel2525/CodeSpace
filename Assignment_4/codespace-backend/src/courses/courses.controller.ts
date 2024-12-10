2b415a2db1a0e7b4fcd0d4c708245969f0ef8a4b
Vishwa1017 <97929298+Vishwa1017@users.noreply.github.com>
2024-12-10 07:24:18 -0500

    Uncommenting guards and auth

--- Assignment_4/codespace-backend/src/courses/courses.controller.ts
+++ Assignment_4/codespace-backend/src/courses/courses.controller.ts
@@ -21,23 +21,23 @@ import { JwtAuthGuard } from 'src/auth/guards/jwt-auth/jwt-auth.guard';
 @Controller('courses')
 export class CoursesController {
   constructor(private readonly courseService: CourseService) {}
-  // @Roles(UserRole.CODER, UserRole.INSTRUCTOR)
-  // @UseGuards(RolesGuard)
-  // @UseGuards(JwtAuthGuard)
+  @Roles(UserRole.CODER, UserRole.INSTRUCTOR)
+  @UseGuards(RolesGuard)
+  @UseGuards(JwtAuthGuard)
   @Get()
   getAllCourses() {
     return this.courseService.findAll();
   }
-  // @Roles(UserRole.CODER, UserRole.INSTRUCTOR)
-  // @UseGuards(RolesGuard)
-  // @UseGuards(JwtAuthGuard)
+  @Roles(UserRole.CODER, UserRole.INSTRUCTOR)
+  @UseGuards(RolesGuard)
+  @UseGuards(JwtAuthGuard)
   @Get('details/:courseId')
   async findOne(@Param('courseId') courseId: string) {
     return this.courseService.findOne(courseId);
   }
-  // @Roles(UserRole.CODER, UserRole.INSTRUCTOR)
-  // @UseGuards(RolesGuard)
-  // @UseGuards(JwtAuthGuard)
+  @Roles(UserRole.CODER, UserRole.INSTRUCTOR)
+  @UseGuards(RolesGuard)
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createCourseDto: CreateCourseDto) {
     return this.courseService.create(createCourseDto);
@@ -54,9 +54,9 @@ export class CoursesController {
     return this.courseService.update(courseId, updateCourseDto);
   }
 
-  // @Roles(UserRole.ADMIN, UserRole.INSTRUCTOR)
-  // @UseGuards(RolesGuard)
-  // @UseGuards(JwtAuthGuard)
+  @Roles(UserRole.ADMIN, UserRole.INSTRUCTOR)
+  @UseGuards(RolesGuard)
+  @UseGuards(JwtAuthGuard)
   @Delete(':courseId')
   async remove(@Param('courseId') courseId: string) {
     return this.courseService.remove(courseId);
